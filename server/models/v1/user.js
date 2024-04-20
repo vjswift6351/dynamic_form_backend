bf667ec234d851900fc566aa957306e6df309a58
vijay <g.vijay946@gamil.com>
2024-04-20 11:40:55 +0530

    user update

--- server/models/v1/user.js
+++ server/models/v1/user.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
     username: String,
     email: String,
     password: String,
+    isadmin:String
 });
 
 module.exports = mongoose.model('usertable' , userSchema, 'usertable');
--- server/routes/api/v1/user/index.js
+++ server/routes/api/v1/user/index.js
@@ -21,6 +21,7 @@ router.post('/adduser', async function(req, res, next){
     UserModel.username= req.body.username
     UserModel.email= req.body.email
     UserModel.password= req.body.password
+    UserModel.isadmin= false 
     const newUser =  await UserModel.save();
     res.status(200).json({'message': "User added","data": newUser});
 });
@@ -34,7 +35,7 @@ router.delete('/deleteUser/:id', async function(req, res, next){
 router.put('/updateuser/:id', async function(req, res, next){
     console.log(req.body.id)
     let updateUser = await userModel.findOneAndUpdate({"_id": req.params.id} , {
-        $set: {"username": req.body.username, "email": req.body.email, "password": req.body.password},
+        $set: {"username": req.body.username, "email": req.body.email, "password": req.body.password, "isadmin": false},
     },{new:true})
     console.log(updateUser)
     res.status(200).json({'message': "User Updated","data": updateUser});
